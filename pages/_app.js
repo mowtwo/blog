5d90b28a25fb44bdbbc0993927d06944e98a1dac
chenwencheng <chenwencheng@4399inc.com>
2022-01-04 14:27:24 +0800

    修改代码高亮

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,13 @@ import Head from 'next/head'
 
 import '../styles/main.css'
 
+import Prism from 'prism-react-renderer/prism'
+;(typeof global !== 'undefined' ? global : window).Prism = Prism
+
+require('prismjs/components/prism-typescript')
+require('prismjs/components/prism-bash')
+require('prismjs/components/prism-json')
+
 export default function Nextra({ Component, pageProps }) {
   // console.log(pageProps)
   return (
